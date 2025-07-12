4fa029831b6137b771668498ed0d3e5fcb2404c6
boromyr <52782032+boromyr@users.noreply.github.com>
2025-07-13 00:49:33 +0200

    merge.bat

--- .merge.bat
+++ .merge.bat
@@ -1,9 +1,11 @@
 git pull upstream main --progress -v
 
-git fetch https://github.com/SoftFever/OrcaSlicer.git pull/9481/head
-git merge FETCH_HEAD
+@REM git fetch https://github.com/SoftFever/OrcaSlicer.git pull/10027/head
+@REM git merge FETCH_HEAD
 @REM git fetch https://github.com/SoftFever/OrcaSlicer.git pull/8181/head
 @REM git merge FETCH_HEAD
-git fetch https://github.com/SoftFever/OrcaSlicer.git pull/8184/head
-git merge FETCH_HEAD
+@REM git fetch https://github.com/SoftFever/OrcaSlicer.git pull/9773/head
+@REM git merge FETCH_HEAD
+@REM git fetch https://github.com/SoftFever/OrcaSlicer.git pull/9998/head
+@REM git merge FETCH_HEAD
 
