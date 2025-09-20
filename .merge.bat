7c09f498d47bc5bed1fb67119d3c318f264e395c
boromyr <52782032+boromyr@users.noreply.github.com>
2025-09-20 15:54:43 +0200

    .

---
+++ .merge.bat
@@ -0,0 +1,68 @@
+git pull upstream --progress -v
+pause
+@REM git fetch https://github.com/SoftFever/OrcaSlicer.git pull/10027/head
+@REM git merge FETCH_HEAD
+@REM pause@REM git fetch https://github.com/SoftFever/OrcaSlicer.git pull/8181/head
+@REM git merge FETCH_HEAD
+@REM pause@REM git fetch https://github.com/SoftFever/OrcaSlicer.git pull/9773/head
+@REM git merge FETCH_HEAD
+@REM pause@REM git fetch https://github.com/SoftFever/OrcaSlicer.git pull/9998/head
+@REM git merge FETCH_HEAD
+@REM pause
+@REM * -------------------------------- comp -----------------------------------  
+@REM git fetch https://github.com/SoftFever/OrcaSlicer.git pull/10654/head
+@REM git merge FETCH_HEAD
+@REM pause
+@REM git fetch https://github.com/SoftFever/OrcaSlicer.git pull/10653/head
+@REM git merge FETCH_HEAD
+@REM pause
+@REM git fetch https://github.com/SoftFever/OrcaSlicer.git pull/10750/head
+@REM git merge FETCH_HEAD
+@REM pause
+@REM git fetch https://github.com/SoftFever/OrcaSlicer.git pull/10729/head
+@REM git merge FETCH_HEAD
+@REM pause
+@REM git fetch https://github.com/SoftFever/OrcaSlicer.git pull/10727/head
+@REM git merge FETCH_HEAD
+@REM pause
+@REM git fetch https://github.com/SoftFever/OrcaSlicer.git pull/10707/head
+@REM git merge FETCH_HEAD
+@REM pause
+@REM git fetch https://github.com/SoftFever/OrcaSlicer.git pull/10703/head
+@REM git merge FETCH_HEAD
+@REM pause
+@REM git fetch https://github.com/SoftFever/OrcaSlicer.git pull/10699/head
+@REM git merge FETCH_HEAD
+@REM pause
+@REM git fetch https://github.com/SoftFever/OrcaSlicer.git pull/10670/head
+@REM git merge FETCH_HEAD
+@REM pause
+@REM * -------------------------------- all -----------------------------------  
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/10641/head
+git merge FETCH_HEAD
+pause
+@REM git fetch https://github.com/SoftFever/OrcaSlicer.git pull/10704/head
+@REM git merge FETCH_HEAD
+@REM pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/10665/head
+git merge FETCH_HEAD
+pause
+@REM @REM git fetch https://github.com/SoftFever/OrcaSlicer.git pull/10350/head
+@REM @REM git merge FETCH_HEAD
+@REM @REM pause
+@REM git fetch https://github.com/SoftFever/OrcaSlicer.git pull/10158/head
+@REM git merge FETCH_HEAD
+@REM pause
+@REM git fetch https://github.com/SoftFever/OrcaSlicer.git pull/9998/head
+@REM git merge FETCH_HEAD
+@REM pause
+@REM git fetch https://github.com/SoftFever/OrcaSlicer.git pull/10027/head
+@REM git merge FETCH_HEAD
+@REM pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/10722/head
+git merge FETCH_HEAD
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/10758/head
+git merge FETCH_HEAD
+pause
+
