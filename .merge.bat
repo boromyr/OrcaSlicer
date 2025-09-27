30f648fd6448fd69e3d44c99010590666af2274d
boromyr <52782032+boromyr@users.noreply.github.com>
2025-09-27 19:17:02 +0200

    .

---
+++ .merge.bat
@@ -0,0 +1,13 @@
+git pull upstream main --progress -v
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/10641/head
+git merge FETCH_HEAD
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/10665/head
+git merge FETCH_HEAD
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/10722/head
+git merge FETCH_HEAD
+pause
+
+
---
+++ reset.bat
@@ -0,0 +1,3 @@
+git fetch upstream & git checkout main & git reset --hard upstream/main & git clean -fdx & git push origin main --force & xcopy D:\Users\PC\Desktop\.github\workflows\ D:\Users\PC\Documents\GitHub\OrcaSlicer\.github\workflows /s /e /y
+@REM & git apply D:\Users\PC\Documents\GitHub\0001-.patch
+@REM git format-patch -1
