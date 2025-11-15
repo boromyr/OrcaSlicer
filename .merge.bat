5ee830faed7c76f823e34addb2d0cb47c4e0b590
boromyr <52782032+boromyr@users.noreply.github.com>
2025-11-15 21:14:21 +0100

    . [skip ci]

---
+++ .merge.bat
@@ -0,0 +1,24 @@
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/10947/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11015/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11196/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11065/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11220/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11233/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11249/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11255/head
+git merge FETCH_HEAD --no-edit
+pause
