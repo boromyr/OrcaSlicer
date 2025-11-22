3985a78dea9ee5a3981a7ca2503a7dbf3565d3d0
boromyr <52782032+boromyr@users.noreply.github.com>
2025-11-22 17:13:18 +0100

    h

--- .merge.bat
+++ .merge.bat
@@ -1,16 +1,13 @@
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11220/head
+git merge FETCH_HEAD --no-edit
+pause
 git fetch https://github.com/SoftFever/OrcaSlicer.git pull/10947/head
 git merge FETCH_HEAD --no-edit
 pause
 git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11015/head
 git merge FETCH_HEAD --no-edit
 pause
-@REM git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11196/head
-@REM git merge FETCH_HEAD --no-edit
-@REM pause
 git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11065/head
-git merge FETCH_HEAD --no-edit
-pause
-git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11220/head
 git merge FETCH_HEAD --no-edit
 pause
 git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11233/head
@@ -22,27 +19,9 @@ pause
 git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11255/head
 git merge FETCH_HEAD --no-edit
 pause
-git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11403/head
-git merge FETCH_HEAD --no-edit
-pause
 git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11407/head
 git merge FETCH_HEAD --no-edit
 pause
-git fetch https://github.com/SoftFever/OrcaSlicer.git pull/10781/head
-git merge FETCH_HEAD --no-edit
-pause
-git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11401/head
-git merge FETCH_HEAD --no-edit
-pause
-git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11233/head
-git merge FETCH_HEAD --no-edit
-pause
-git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11211/head
-git merge FETCH_HEAD --no-edit
-pause
-git fetch https://github.com/SoftFever/OrcaSlicer.git pull/9907/head
-git merge FETCH_HEAD --no-edit
-pause
-git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11430/head
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11403/head
 git merge FETCH_HEAD --no-edit
 pause
