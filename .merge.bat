7fcf1aa66cb6db80d6b2898aa0f0259493c92c7f
boromyr <davideromeo.g@gmail.com>
2026-01-11 18:55:28 +0100

    . [skip ci]

---
+++ .merge.bat
@@ -0,0 +1,27 @@
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11015/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11065/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/10565/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11255/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11841/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/10781/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11716/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11681/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11535/head
+git merge FETCH_HEAD --no-edit
+pause & D:\Users\PC\Documents\GitHub\OrcaSlicer\build_release_vs.bat
