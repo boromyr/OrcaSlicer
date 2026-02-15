50b404036c3da6dca5e9fbc12107deebc7ee87c7
boromyr <davideromeo.g@gmail.com>
2026-02-15 21:35:24 +0100

    . [skip ci]

---
+++ .merge.bat
@@ -0,0 +1,79 @@
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11065/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11015/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/10565/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11255/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11535/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11819/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11925/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11786/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/12044/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11899/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11526/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/12057/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/12087/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/12069/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11879/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/12068/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/12115/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/12089/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/11819/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/12113/head
+git merge FETCH_HEAD --no-edit
+pause
+@REM git fetch https://github.com/SoftFever/OrcaSlicer.git pull/12148/head
+@REM git merge FETCH_HEAD --no-edit
+@REM pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/12151/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/12187/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/12055/head
+git merge FETCH_HEAD --no-edit
+pause
+git fetch https://github.com/SoftFever/OrcaSlicer.git pull/12109/head
+git merge FETCH_HEAD --no-edit
+pause
+@REM git fetch https://github.com/SoftFever/OrcaSlicer.git pull/12225/head
+@REM git merge FETCH_HEAD --no-edit
+@REM pause
+D:\Users\PC\Documents\GitHub\OrcaSlicer\build_release_vs.bat
